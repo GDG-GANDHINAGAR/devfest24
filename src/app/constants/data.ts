4f72228c2abf9e49111a41342ab4cdd25ac76424
Parth Jansari <parthjansari@outlook.com>
2024-10-17 03:30:53 +0530

    Update data.ts

--- src/app/constants/data.ts
+++ src/app/constants/data.ts
@@ -81,7 +81,7 @@ export const TICKETS: TicketType[] = [
       'Access to AI/ML Workshop',
       'Workshop Certificate',
     ],
-    soldOut: true,
+    soldOut: false,
     link: 'https://konfhub.com/checkout/devfest-gnr-24?ticketId=23969|1',
     price: {
       footnote: 'limited, until sold out',
